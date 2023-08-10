8efeeca804b4c06c4ca78d7e751e066bc0ef130a
SpencerlElswick <spencerlelswick@gmail.com>
2023-08-10 14:38:06 -0400

    Stub create char functionality

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,20 +1,27 @@
-const Character = require ("../models/character")
+const Character = require("../models/character")
 const { render } = require("ejs");
 
 module.exports = {
-    index,
-    new: newChar,
-    create
+  index,
+  new: newChar,
+  create
 }
 
-function index (req,res){
-    res.render('characters/index', {title: ""})
+function index(req, res) {
+  res.render('characters/index', { title: "" })
 }
 
-function newChar(req,res){
-    res.render('characters/new', {title: ""})
+function newChar(req, res) {
+  res.render('characters/new', { title: "" })
 }
 
-function create(req,res){
-    res.redirect('/characters')
+function create(req, res) {
+  const charData = { ...req.body };
+  try {
+    console.log(charData)
+    res.redirect('/characters/')
+  } catch (err) {
+    console.log(err)
+    res.render("characters/new", { title: 'Create a character', errorMsg: err.message })
+  }
 }
--- models/character.js
+++ models/character.js
@@ -1,98 +1,97 @@
 // 15 + 14 + 13 + 12 + 10 + 8
+// const character = [
+//   {
+//     name: 'elvenatto',
+//     race: 'Elf', // many races for one character
+//     class: 'ranger', // many classes for one character
+//     img: 'https://i.imgur.com/mtskKQf.jpeg',
+//     stats: {
+//       strength: 10,
+//       dexterity: 10,
+//       constitution: 10,
+//       intelligence: 10,
+//       wisdom: 10,
+//       charisma: 10,
+//     },
+//     playerName: 'Spencer', // many characters for one player
+//     items: ['bow', 'dagger', 'quiver'], // one character has many items
+//     misc: {}
+//   }]
 
-const character = [
-  {
-    name: 'elvenatto',
-    race: 'Elf', // many races for one character
-    class: 'ranger', // many classes for one character
-    img: 'https://i.imgur.com/mtskKQf.jpeg',
-    stats: {
-      strength: 10,
-      dexterity: 10,
-      constitution: 10,
-      intelligence: 10,
-      wisdom: 10,
-      charisma: 10,
-    },
-    playerName: 'Spencer', // many characters for one player
-    items: ['bow', 'dagger', 'quiver'], // one character has many items
-    misc: {}
-  }]
-
-const Races = {
-  elf: {
-    name: 'Elf',
-    bonus: {
-      strength: 0,
-      dexterity: 2,
-      constitution: 0,
-      intelligence: 0,
-      wisdom: 0,
-      charisma: 0,
-    }
-  },
-  orc: {
-    name: 'halforc',
-    bonus: {
-      strength: 2,
-      dexterity: 0,
-      constitution: 0,
-      intelligence: 0,
-      wisdom: 0,
-      charisma: 0,
-    }
-  },
-  dwarf: {
-    name: 'dwarf',
-    bonus: {
-      strength: 0,
-      dexterity: 0,
-      constitution: 2,
-      intelligence: 0,
-      wisdom: 0,
-      charisma: 0,
-    }
-  },
-
+// const Races = {
+//   elf: {
+//     name: 'Elf',
+//     bonus: {
+//       strength: 0,
+//       dexterity: 2,
+//       constitution: 0,
+//       intelligence: 0,
+//       wisdom: 0,
+//       charisma: 0,
+//     }
+//   },
+//   orc: {
+//     name: 'halforc',
+//     bonus: {
+//       strength: 2,
+//       dexterity: 0,
+//       constitution: 0,
+//       intelligence: 0,
+//       wisdom: 0,
+//       charisma: 0,
+//     }
+//   },
+//   dwarf: {
+//     name: 'dwarf',
+//     bonus: {
+//       strength: 0,
+//       dexterity: 0,
+//       constitution: 2,
+//       intelligence: 0,
+//       wisdom: 0,
+//       charisma: 0,
+//     }
+//   },
 
 
-}
 
-const Classes = {
-  ranger: {
-    'name': 'ranger',
-    minScore: {
-      strength: 0,
-      dexterity: 12,
-      constitution: 0,
-      intelligence: 0,
-      wisdom: 0,
-      charisma: 0,
-    },
-    proficiency: ['bows', 'daggers']
-  },
-  warrior: {
-    name: 'warrior',
-    minScore: {
-      strength: 12,
-      dexterity: 0,
-      constitution: 0,
-      intelligence: 0,
-      wisdom: 0,
-      charisma: 0,
-    },
-    proficiency: ['swords', 'axes']
-  },
-  wizard: {
-    name: 'wizard',
-    minScore: {
-      strength: 0,
-      dexterity: 0,
-      constitution: 0,
-      intelligence: 12,
-      wisdom: 0,
-      charisma: 0,
-    },
-    proficiency: ['staffs', 'wands']
-  },
-}
+// }
+
+// const Classes = {
+//   ranger: {
+//     'name': 'ranger',
+//     minScore: {
+//       strength: 0,
+//       dexterity: 12,
+//       constitution: 0,
+//       intelligence: 0,
+//       wisdom: 0,
+//       charisma: 0,
+//     },
+//     proficiency: ['bows', 'daggers']
+//   },
+//   warrior: {
+//     name: 'warrior',
+//     minScore: {
+//       strength: 12,
+//       dexterity: 0,
+//       constitution: 0,
+//       intelligence: 0,
+//       wisdom: 0,
+//       charisma: 0,
+//     },
+//     proficiency: ['swords', 'axes']
+//   },
+//   wizard: {
+//     name: 'wizard',
+//     minScore: {
+//       strength: 0,
+//       dexterity: 0,
+//       constitution: 0,
+//       intelligence: 12,
+//       wisdom: 0,
+//       charisma: 0,
+//     },
+//     proficiency: ['staffs', 'wands']
+//   },
+// }
